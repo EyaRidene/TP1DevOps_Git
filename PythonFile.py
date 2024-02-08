c011fe661c7a286e26ebcefeb89350cc39e6d956
eya ridene <eya.ridene@insat.u-carthage.tn>
2024-02-08 22:16:27 +0100

    files modified

---
+++ PythonFile.py
@@ -0,0 +1,5 @@
+num1 = float(input("Entrez le premier nombre: "))
+num2 = float(input("Entrez le second nombre: "))
+
+somme = num1 + num2
+print(f"La somme de {num1} et {num2} est {somme}")
---
+++ PythonFile3.py
@@ -0,0 +1,4 @@
+longueur = 10
+largeur = 5
+aire = longueur * largeur
+print(f"L'aire du rectangle est {aire}")
